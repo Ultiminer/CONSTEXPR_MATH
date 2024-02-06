3416f8dd30105cc91e99bf2f3472d3fdb0c52fa3
FloDev <43553244+Ultiminer@users.noreply.github.com>
2024-02-06 12:58:43 +0100

    Added the nice_cpp_macros header, that provides operations for bit manipulation and such

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include <math.h>
 #include "quick_math.h"
 #include <array>
+#include "nice_cpp_macros.h"    
 int main(int argc, char**argv)
 {
-    
+    float x=8;
+    int l=NCM_GetBitL(x,30,8);
+    std::cout<<l-127;
 
 
     return 0;
---
+++ nice_cpp_macros.h
@@ -0,0 +1,17 @@
+#ifndef NICE_CPP_MACROS_H_
+#define NICE_CPP_MACROS_H_
+
+/*Combining the wisdom acquired from the creation of the quick_math header into a helper library*/
+
+#define NCM_LENGTH(x) ((sizeof(x))/(sizeof(*(x))))
+#define NCM_FloatExponent(f) (((*((unsigned int*)&(f)) & 0x7f800000)>>23)-127)
+#define NCM_FloatMantissa(f)((*((unsigned int*)&(f)) & 0x007fffff))
+#define NCM_POW2(x) ((1<<23)*(127+(x)))
+#define NCM_FLOAT_MULTIPLIER(x) (NCM_FloatMantissa((x))+(1<<23)*127)
+#define NCM_Swap(x,y)((x)=(x)+(y); (y)=(x)-(y);(x)=(x)-(y);)
+#define NCM_SignBit(x) (((*(unsigned int*)&(x))&(1<<31))>>31)
+#define NCM_GetBit(x,bitPos) ((((*(unsigned int*)&(x))&(1<<(bitPos))))>>(bitPos))
+#define NCM_GetBitL(x,bitPos, length) ((((*(unsigned int*)&(x))<<(31-bitPos)))>>(32-length))
+
+
+#endif
